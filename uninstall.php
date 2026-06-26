02622b6f14c7ea3d2f73f8104eba1a8ead42fae5
CoreyJSteiner <corey.joseph.steiner@gmail.com>
2026-06-26 11:19:15 -0400

    pcp updates

--- uninstall.php
+++ uninstall.php
@@ -12,9 +12,9 @@
 defined( 'WP_UNINSTALL_PLUGIN' ) || exit;
 
 if ( is_multisite() ) {
-	$site_ids = get_sites( [ 'fields' => 'ids', 'number' => 0 ] );
-	foreach ( $site_ids as $site_id ) {
-		switch_to_blog( $site_id );
+	$rvpbi_site_ids = get_sites( [ 'fields' => 'ids', 'number' => 0 ] );
+	foreach ( $rvpbi_site_ids as $rvpbi_site_id ) {
+		switch_to_blog( $rvpbi_site_id );
 		delete_option( 'powerbi_settings' );
 		restore_current_blog();
 	}
